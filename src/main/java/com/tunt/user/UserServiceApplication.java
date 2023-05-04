8f9b85d7f6fc0c39ed7cdce590b29097342cd8c2
TuNT <nguyentuantu2101@gmail.com>
2023-05-04 22:33:06 +0700

    update

--- src/main/java/com/tunt/user/UserServiceApplication.java
+++ src/main/java/com/tunt/user/UserServiceApplication.java
@@ -1,21 +1,13 @@
 package com.tunt.user;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserServiceApplication extends SpringBootServletInitializer {
+public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
     }
 
 }
